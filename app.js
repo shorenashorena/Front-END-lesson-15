7c09d66d2ac731ee6f27def09d34749b44fc05e2
shorenashorena <shorenabad@gmail.com>
2025-11-11 01:40:07 +0400

    js

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log("Hello, World!");
+console.log("Hello, World!", "Hello, World!");
+
+
+let productquanity = 5;
+const productprice = 10;
+
+
+productQuantity =15
+
+console.log(productQuantity);
+
+const firstName ="შორენა";
+const lastName ="ბადურაშვილი";
+console.log(firstName, lastName);
+
+const fullName = `${firstName} ${lastName}`;
+
+const greeting = `გამარჯობა, ჩემი სახელია ${fullName}!დღეს არის ორშაბათი, 22:00, მე ძალიან მიყვარს სიახლეების გაცნობა და შემდეგ ჩემი მოსწავლეებისთვის გადაცემა`;
+console.log(greeting);
